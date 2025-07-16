e3f7ed31cd03ef132858adb01412a5e0420e6c53
Standly Laguerre <stanlaguerre3@gmail.com>
2025-07-16 00:38:59 -0500

    Add base to vite.config.js for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/spidr-airfryer-form/', // ← this tells Vite to build for your repo subpath
 })
